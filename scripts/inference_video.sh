99668f5eec97b76c24247a54e2bef871b46b7266
Your Name <you@example.com>
2026-03-05 17:18:23 +0800

    zhelun

--- scripts/inference_video.sh
+++ scripts/inference_video.sh
@@ -1,6 +1,14 @@
-python examples/wanvideo/test_swap.py \
+# python examples/wanvideo/test_swap.py \
+#     --model_path models/GenHOI_wan_flf.consolidated \
+#     --output_dir results/swap_81 \
+#     --data_csv data/long_video_swap/swap.csv \
+#     --gpus 2 \
+#     --max_num_frames 81
+
+python examples/wanvideo/test_selfswap.py \
     --model_path models/GenHOI_wan_flf.consolidated \
-    --output_dir results/swap_81 \
-    --data_csv data/long_video_swap/swap.csv \
+    --output_dir results/selfswap_81 \
+    --data_csv data/AnchorCrafter-400_405f/dataset_select_f50.csv \
     --gpus 2 \
-    --max_num_frames 81
+    --max_num_frames 81 \
+    --is_fl
