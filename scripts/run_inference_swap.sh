905c2972f1b3a18d02ca81fc61c74fd60059597e
gallenszl <gallenszl@github.com>
2026-04-03 11:14:22 +0800

    revise_bug

--- scripts/run_inference_swap.sh
+++ scripts/run_inference_swap.sh
@@ -1,10 +1,10 @@
-# python examples/wanvideo/swap_infer.py \
-#     --output_dir results/swap_vace_ff_debug \
-#     --data_csv data/long_video_swap/swap.csv \
-#     --gpus 0 \
-#     --max_num_frames 81 \
-#     --model_path models/GenHOI_VACE/step-5000-gate_attn.safetensors \
-#     --lora_path models/GenHOI_VACE/step-1700-lora-gate-720.safetensors
+python examples/wanvideo/swap_infer.py \
+    --output_dir results/swap_vace_cross_me \
+    --data_csv /root/paddlejob/workspace/shenzhelun/data/gongyingshang_cross_our_paths_bbox_replaced.csv \
+    --gpus 0,1,2,3,4,5,6,7 \
+    --max_num_frames 81 \
+    --model_path models/GenHOI_VACE/step-5000-gate_attn.safetensors \
+    --lora_path models/GenHOI_VACE/step-1700-lora-gate-720.safetensors
 
 
 # python examples/wanvideo/selfswap_infer.py \
@@ -15,10 +15,10 @@
 #     --model_path models/GenHOI_VACE/step-5000-gate_attn.safetensors \
 #     --lora_path models/GenHOI_VACE/step-1700-lora-gate-720.safetensors
 
-python examples/wanvideo/selfswap_infer.py \
-    --output_dir results/selfswap_demo \
-    --data_csv data/AnchorCrafter-400_405f/dataset_select_f50.csv \
-    --gpus 0,1,2,3,4,5,6,7 \
-    --max_num_frames 81 \
-    --model_path models/GenHOI_VACE/step-5000-gate_attn.safetensors \
-    --lora_path models/GenHOI_VACE/step-1700-lora-gate-720.safetensors
+# python examples/wanvideo/selfswap_infer.py \
+#     --output_dir results/selfswap_demo \
+#     --data_csv data/AnchorCrafter-400_405f/dataset_select_f50.csv \
+#     --gpus 0,1,2,3,4,5,6,7 \
+#     --max_num_frames 81 \
+#     --model_path models/GenHOI_VACE/step-5000-gate_attn.safetensors \
+#     --lora_path models/GenHOI_VACE/step-1700-lora-gate-720.safetensors
